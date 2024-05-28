094b6a635581d0bd396446500f54083414792987
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-05-28 17:12:00 +0200

    Add Gameboard factory that creates board

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+function Gameboard() {
+    const board = []
+
+    for (let i = 0; i < 9; i++) {
+        board.push(Square());
+    }
+
+    const getBoard = board;
+
+    return { getBoard };
+}
+
+/* function Square() {
+    let value = 0;
+    const getValue = () => value;
+
+    const changeValue = (player) => {
+        value = player.marker;
+    }
+
+    return { getValue, changeValue };
+} */
+
 function Players(playerOneName = "Player 1", playerTwoName = "Player 2") {
     const createPlayers = [
         {
