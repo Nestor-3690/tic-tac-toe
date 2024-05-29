d2386b810d614f9b79a395c11413cfc36670eb7d
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-05-29 10:04:05 +0200

    Add verification that the value is not initial

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ function Gameboard() {
     }
 
     const gameWon = () => {
-        if (board[0].getValue() === board[3].getValue() && board[3].getValue() === board[6].getValue() || board[1].getValue() === board[4].getValue() && board[4].getValue() === board[7].getValue() || board[2].getValue() === board[5].getValue() && board[5].getValue() === board[8].getValue() ||
-        board[0].getValue() === board[1].getValue() && board[1].getValue() === board[2].getValue() || board[3].getValue() === board[4].getValue() && board[4].getValue() === board[5].getValue() || board[6].getValue() === board[7].getValue() && board[7].getValue() === board[8].getValue() ||
-        board[0].getValue() === board[4].getValue() && board[4].getValue() === board[8].getValue() || board[6].getValue() === board[4].getValue() && board[4].getValue() === board[2].getValue()) {
+        if (board[0].getValue() === board[3].getValue() && board[3].getValue() === board[6].getValue() && board[0].getValue() !== 0 || board[1].getValue() === board[4].getValue() && board[4].getValue() === board[7].getValue() && board[1].getValue() !== 0 || board[2].getValue() === board[5].getValue() && board[5].getValue() === board[8].getValue() && board[2].getValue() !== 0 ||
+        board[0].getValue() === board[1].getValue() && board[1].getValue() === board[2].getValue() && board[0].getValue() !== 0 || board[3].getValue() === board[4].getValue() && board[4].getValue() === board[5].getValue() && board[3].getValue() !== 0 || board[6].getValue() === board[7].getValue() && board[7].getValue() === board[8].getValue() && board[6].getValue() !== 0 ||
+        board[0].getValue() === board[4].getValue() && board[4].getValue() === board[8].getValue() && board[0].getValue() !== 0 || board[6].getValue() === board[4].getValue() && board[4].getValue() === board[2].getValue() && board[6].getValue() !== 0) {
             return true;
         } else {
             return false;
